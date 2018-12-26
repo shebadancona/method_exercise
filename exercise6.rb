d42e70670656f8b35c60bbc5e4a5f6260e440d9b
shebadancona <shebadancona@gmail.com>
2018-12-25 23:29:31 -0500

    repeat method

--- exercise6.rb
+++ exercise6.rb
@@ -10,11 +10,41 @@
 
 
 
-def wrap_text (word, symbol)
-    result = symbol + word + symbol
-    return result
+# def wrap_text (word, symbol)
+#     result = symbol + word + symbol
+#     return result
+# end
+
+# a = wrap_text("new message","###")
+# b = wrap_text(a, "===")
+# puts wrap_text(b, "---")
+
+
+
+counter = 1
+def speed(number) 
+    puts "How far did person #{number} run (in meters)?"
+    distance = gets.to_f
+    puts "How long (in minutes)did person #{number}take to run #{distance} meters?"
+    min = gets.to_f
+    sec = min * 60
+    return distance/sec
 end
 
-a = wrap_text("new message","###")
-b = wrap_text(a, "===")
-puts wrap_text(b, "---")
+speed1 = speed(counter)
+counter +=1
+speed2 = speed(counter)
+counter +=1 
+speed3 = speed(counter)
+
+if speed3 > speed2 && speed3 > speed1
+    puts "Person 3 was the fastest at #{speed3} m/s"
+  elsif speed2 > speed3 && speed2 > speed1
+    puts "Person 2 was the fastest at #{speed2} m/s"
+  elsif speed1 > speed3 && speed1 > speed2
+    puts "Person 1 was the fastest at #{speed1} m/s"
+  elsif speed1 == speed2 && speed2 == speed3
+    puts "Everyone tied at #{speed1} m/s"
+  else
+    puts "Well done everyone!"
+  end
