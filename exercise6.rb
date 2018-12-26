94e31ea8b2081b9aa3f03d697caa7f83b650b9d4
shebadancona <shebadancona@gmail.com>
2018-12-25 22:28:33 -0500

    wrap_text

--- exercise6.rb
+++ exercise6.rb
@@ -1,12 +1,20 @@
-puts "what is the temperature in Fahrenheit?"
-tempf = gets.chomp.to_i
+# puts "what is the temperature in Fahrenheit?"
+# tempf = gets.chomp.to_i
 
-def temp_to_celsius (temp)
-    return (temp - 32) * 5/9 
-end 
-
-
-puts "the temperature is #{temp_to_celsius (tempf)} celsius"
+# def temp_to_celsius (temp)
+#     return (temp - 32) * 5/9 
+# end 
 
 
+# puts "the temperature is #{temp_to_celsius (tempf)} celsius"
 
+
+
+def wrap_text (word, symbol)
+    result = symbol + word + symbol
+    return result
+end
+
+a = wrap_text("new message","###")
+b = wrap_text(a, "===")
+puts wrap_text(b, "---")
